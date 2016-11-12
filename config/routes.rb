ad20ed9e25dac811e43a0c7a4edff700aae5b98f
IuliaUngur <iuli_ungur@yahoo.com>
2016-11-12 23:57:27 +0200

    Controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+
+  resources :readings, controller: 'readings'
+  resources :sensors, controller: 'sensors'
+  resources :version_sets, controller: 'version_sets'
 end
