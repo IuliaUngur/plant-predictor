7a1b4eaa8bd8a84a1ec4353fdf6e3dc959d7dff9
Iulia Ungur <iulia.ungur@pitechnologies.ro>
2017-04-11 12:14:08 +0300

    CSV Export task for predictions

---
+++ app/services/ap/format_generators/export_csv.rb
@@ -0,0 +1,35 @@
+require 'csv'
+
+module Ap
+  module FormatGenerators
+    class ExportCsv
+      def initialize(environment, plant)
+        @predictions = Prediction.predictions_on_plants(environment, plant)
+        @plant = plant
+        @environment = environment
+        @path = "#{Rails.root}/public/exports/#{@environment}_#{@plant}.csv"
+      end
+
+      def perform
+        csv_generator
+      end
+
+      private
+
+      def csv_generator(options = {})
+        CSV.open(@path, "wb") do |csv|
+          csv << ["id", "light", "temperature", "vibration",
+            "humidity", "raindrop", "distance", "result"]
+
+          @predictions.each do |prediction|
+            data=[]
+            data << prediction.id
+            prediction.sensors.each {|sensor| data << sensor.value }
+            data << prediction.result
+            csv << data
+          end
+        end
+      end
+    end
+  end
+end
