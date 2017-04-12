4ecd02da7c3bcf846f81b10f866a8dd3b1b06ab5
Iulia Ungur <iulia.ungur@pitechnologies.ro>
2017-04-12 14:06:00 +0300

    Fselector service and task final version

--- app/services/ap/algorithm/fselector_filter.rb
+++ app/services/ap/algorithm/fselector_filter.rb
@@ -6,33 +6,55 @@ module Ap
       end
 
       def perform
-
-        puts "\n>============> #{File.basename(__FILE__)}"
-        r = FSelector::IG.new
-
-        name2type = {
-            :light => :string,
-            :temperature => :string,
-            :vibration => :numeric,
-            :humidity => :numeric,
-            :raindrop => :string,
-            :distance => :string
-        }
-
-        r.data_from_csv("public/exports/#{@file}.csv", feature_name2type: name2type)
-        puts "  number of samples read: %d" % r.get_sample_size
-
-        puts '  # features: ' + r.get_features.size.to_s
-        puts "  IG(light) = #{r.get_feature_scores[:light][:BEST]}"
-        puts "  IG(temperature) = #{r.get_feature_scores[:temperature][:BEST]}"
-        puts "  IG(vibration) = #{r.get_feature_scores[:vibration][:BEST]}"
-        puts "  IG(humidity) = #{r.get_feature_scores[:humidity][:BEST]}"
-        puts "  IG(raindrop) = #{r.get_feature_scores[:raindrop][:BEST]}"
-        puts "  IG(distance) = #{r.get_feature_scores[:distance][:BEST]}"
-
-        puts "<============< #{File.basename(__FILE__)}"
+        initialize_selector
+        process_values
       end
 
+      def scores
+        @scores
+      end
+
+      def ranks
+        @ranks
+      end
+
+      private
+
+      def initialize_selector
+        @r = FSelector::IG.new
+
+        name2type = {
+          light: :string,
+          temperature: :string,
+          vibration: :numeric,
+          humidity: :numeric,
+          raindrop: :string,
+          distance: :string
+        }
+
+        @r.data_from_csv("public/exports/#{@file}.csv", feature_name2type: name2type)
+      end
+
+      def process_values
+        @scores = empty_features
+        @ranks = empty_features
+
+        empty_features.keys.each do |feature|
+          @scores[feature] = @r.get_feature_scores[feature][:BEST]
+          @ranks[feature] = @r.get_feature_ranks[feature]
+        end
+      end
+
+      def empty_features
+        {
+          light: '',
+          temperature: '',
+          vibration: '',
+          humidity: '',
+          raindrop: '',
+          distance: ''
+        }
+      end
     end
   end
 end
--- lib/tasks/fselector_test.rake
+++ lib/tasks/fselector_test.rake
@@ -5,5 +5,42 @@ task fselector: :environment do
   file = (ENV['FILE'] || 'simulation_plant')
 
   puts "file= #{file}"
-  Ap::Algorithm::FselectorFilter.new(file).perform
+  test_fselect(file)
+end
+
+def test_fselect(file)
+  puts "\n>============> #{File.basename(__FILE__)}"
+  r = FSelector::IG.new
+
+  name2type = {
+      :light => :string,
+      :temperature => :string,
+      :vibration => :numeric,
+      :humidity => :numeric,
+      :raindrop => :string,
+      :distance => :string
+  }
+
+  r.data_from_csv("public/exports/#{file}.csv", feature_name2type: name2type)
+  puts "  number of samples read: %d" % r.get_sample_size
+
+  puts '  # features: ' + r.get_features.size.to_s
+
+  puts '  Scores '
+  puts "  IG(light) = #{r.get_feature_scores[:light][:BEST]}"
+  puts "  IG(temperature) = #{r.get_feature_scores[:temperature][:BEST]}"
+  puts "  IG(vibration) = #{r.get_feature_scores[:vibration][:BEST]}"
+  puts "  IG(humidity) = #{r.get_feature_scores[:humidity][:BEST]}"
+  puts "  IG(raindrop) = #{r.get_feature_scores[:raindrop][:BEST]}"
+  puts "  IG(distance) = #{r.get_feature_scores[:distance][:BEST]}"
+
+
+  puts '  Ranks'
+  puts "  IG(light) = #{r.get_feature_ranks[:light]}"
+  puts "  IG(temperature) = #{r.get_feature_ranks[:temperature]}"
+  puts "  IG(vibration) = #{r.get_feature_ranks[:vibration]}"
+  puts "  IG(humidity) = #{r.get_feature_ranks[:humidity]}"
+  puts "  IG(raindrop) = #{r.get_feature_ranks[:raindrop]}"
+  puts "  IG(distance) = #{r.get_feature_ranks[:distance]}"
+  puts "<============< #{File.basename(__FILE__)}"
 end
